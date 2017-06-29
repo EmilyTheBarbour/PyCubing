205fb6843b750d01bd5f170f68a7c967e9ce8d47
Emily Barbour <emilythebarbour@gmail.com>
2017-06-29 03:15:45 -0400

    replaced old Solve structure with list structure created main program file

--- puzzle.py
+++ puzzle.py
@@ -2,21 +2,18 @@ class Puzzle:
 
     def __init__(self, name):
         self.name = name
-        self.description,  = ""
-        self.solves, self.shuffles = []
+        self.description  = ""
+        self.solves = []
+        self.shuffles = []
 
+    #a solve is a 2 item list, consistent of [time, shuffle]
     def add_solve(self, solve):
         self.solves.append(solve)
 
-class Solve:
-
-    def __init__(self, time, shuffle):
-        self.time = time
-        self.shuffle = shuffle
 
 
 three_by_three = Puzzle("3x3")
 three_by_three.description = "A cuboid puzzle with 6 different colored faces who all rotate about their central cubie"
-three_by_three.add_solve(Solve(5.5,"test"))
-
-print(three_by_three.solves)
+three_by_three.add_solve([5.5,"test"])
+three_by_three.add_solve([5.5,"test"])
+three_by_three.print_solves()
