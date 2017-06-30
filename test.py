f032dc9d7fc21d255588d6995564f49b1ed6a9c4
Emily Barbour <emilythebarbour@gmail.com>
2017-06-29 23:31:57 -0400

    created directory for shuffle dictionary files

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from puzzle import Puzzle
 
 x = Puzzle("test")
-x.add_shuffles('3x3_shuffles.txt')
+x.add_shuffles('puzzles/3x3_shuffles.txt')
 
 print(len(x.shuffles))
 print(x.generate_shuffle())
